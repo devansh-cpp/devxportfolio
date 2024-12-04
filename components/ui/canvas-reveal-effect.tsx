92655d4b0f39a4972b7ec9c93f012fb6b3e62148
devansh-cpp <113198313+devansh-cpp@users.noreply.github.com>
2024-12-04 20:50:48 +0530

    Portfolio-v1.02

--- components/ui/canvas-reveal-effect.tsx
+++ components/ui/canvas-reveal-effect.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { cn } from "@/lib/utils";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { ESLintConfig } from "next/dist/server/config-shared";
 import React, { useMemo, useRef } from "react";
 import * as THREE from "three";
 
